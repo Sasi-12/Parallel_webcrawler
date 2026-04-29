b891ea50e2f5a906a8e63ff19bff6f348c1f5b51
Sasi367 <sasikumari0604@gmail.com>
2026-04-29 20:51:15 +0530

    commit

--- cd0381-advanced-java-programming-techniques-projectstarter-master/starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
+++ cd0381-advanced-java-programming-techniques-projectstarter-master/starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilerImpl.java
@@ -3,24 +3,18 @@ package com.udacity.webcrawler.profiler;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.io.Writer;
-import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.time.Clock;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Objects;
-import java.util.function.Predicate;
 
 import static java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME;
 
-/**
- * Concrete implementation of the {@link Profiler}.
- */
 final class ProfilerImpl implements Profiler {
 
   private final Clock clock;
@@ -33,56 +27,55 @@ final class ProfilerImpl implements Profiler {
     this.startTime = ZonedDateTime.now(clock);
   }
 
-  private Boolean profiledClass(Class<?> klass) {
-    List<Method> methods = new ArrayList<>(Arrays.asList(klass.getDeclaredMethods()));
-
-    if (methods.isEmpty()) {
-      return false;
-    }
-    return methods.stream().anyMatch(x -> x.getAnnotation(Profiled.class) != null);
+  private boolean hasProfiledMethod(Class<?> klass) {
+    return Arrays.stream(klass.getDeclaredMethods())
+            .anyMatch(method -> method.isAnnotationPresent(Profiled.class));
   }
 
   @Override
   public <T> T wrap(Class<T> klass, T delegate) {
     Objects.requireNonNull(klass);
-    if (!profiledClass(klass)) {
-      throw new IllegalArgumentException(klass.getName() + "doesn't have profiled methods.");
+    Objects.requireNonNull(delegate);
+
+    if (!hasProfiledMethod(klass)) {
+      throw new IllegalArgumentException(
+              klass.getName() + " doesn't have any @Profiled methods.");
     }
 
-    // TODO: Use a dynamic proxy (java.lang.reflect.Proxy) to "wrap" the delegate in a
-    //       ProfilingMethodInterceptor and return a dynamic proxy from this method.
-    //       See https://docs.oracle.com/javase/10/docs/api/java/lang/reflect/Proxy.html.
-    ProfilingMethodInterceptor interceptor = new ProfilingMethodInterceptor(clock, delegate, state, startTime);
+    ProfilingMethodInterceptor interceptor =
+            new ProfilingMethodInterceptor(clock, delegate, state, startTime);
 
     Object proxy = Proxy.newProxyInstance(
-            ProfilerImpl.class.getClassLoader(),
-            new Class[]{klass},
+            klass.getClassLoader(),
+            new Class<?>[]{klass},
             interceptor
     );
 
-    return (T) proxy;
+    return klass.cast(proxy);
   }
 
   @Override
   public void writeData(Path path) {
-    // TODO: Write the ProfilingState data to the given file path. If a file already exists at that
-    //       path, the new data should be appended to the existing file.
     Objects.requireNonNull(path);
 
-    try(Writer writer = Files.newBufferedWriter(path)) {
+    try (Writer writer = Files.newBufferedWriter(
+            path,
+            StandardOpenOption.CREATE,
+            StandardOpenOption.APPEND
+    )) {
       writeData(writer);
-      writer.flush();
-    } catch (IOException ex) {
-      ex.printStackTrace();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
-
   }
 
   @Override
   public void writeData(Writer writer) throws IOException {
     writer.write("Run at " + RFC_1123_DATE_TIME.format(startTime));
     writer.write(System.lineSeparator());
+
     state.write(writer);
+
     writer.write(System.lineSeparator());
   }
 }
--- cd0381-advanced-java-programming-techniques-projectstarter-master/starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
+++ cd0381-advanced-java-programming-techniques-projectstarter-master/starter/webcrawler/src/main/java/com/udacity/webcrawler/profiler/ProfilingMethodInterceptor.java
@@ -9,10 +9,6 @@ import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.util.Objects;
 
-/**
- * A method interceptor that checks whether {@link Method}s are annotated with the {@link Profiled}
- * annotation. If they are, the method interceptor records how long the method invocation took.
- */
 final class ProfilingMethodInterceptor implements InvocationHandler {
 
   private final Clock clock;
@@ -20,38 +16,48 @@ final class ProfilingMethodInterceptor implements InvocationHandler {
   private final ProfilingState state;
   private final ZonedDateTime startTime;
 
-  // TODO: You will need to add more instance fields and constructor arguments to this class.
-  ProfilingMethodInterceptor(Clock clock, Object delegate, ProfilingState state, ZonedDateTime startTime) {
+  ProfilingMethodInterceptor(
+          Clock clock,
+          Object delegate,
+          ProfilingState state,
+          ZonedDateTime startTime) {
+
     this.clock = Objects.requireNonNull(clock);
-    this.delegate = delegate;
-    this.state = state;
-    this.startTime = startTime;
+    this.delegate = Objects.requireNonNull(delegate);
+    this.state = Objects.requireNonNull(state);
+    this.startTime = Objects.requireNonNull(startTime);
   }
 
   @Override
   public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-    // TODO: This method interceptor should inspect the called method to see if it is a profiled
-    //       method. For profiled methods, the interceptor should record the start time, then
-    //       invoke the method using the object that is being profiled. Finally, for profiled
-    //       methods, the interceptor should record how long the method call took, using the
-    //       ProfilingState methods.
-    Object invoked;
+
+    if (method.getDeclaringClass() == Object.class &&
+            method.getName().equals("equals")) {
+      return method.invoke(delegate, args);
+    }
+
+    boolean profiled = method.isAnnotationPresent(Profiled.class);
     Instant start = null;
-    boolean profiled = method.getAnnotation(Profiled.class) != null;
+
     if (profiled) {
       start = clock.instant();
     }
+
     try {
-      invoked = method.invoke(delegate, args);
-    } catch (InvocationTargetException ex) {
-      throw ex.getTargetException();
+      Object result = method.invoke(delegate, args);
+      return result;
+
+    } catch (InvocationTargetException e) {
+      throw e.getCause();
+
+    } catch (IllegalAccessException e) {
+      throw new RuntimeException(e);
+
     } finally {
       if (profiled) {
         Duration duration = Duration.between(start, clock.instant());
         state.record(delegate.getClass(), method, duration);
       }
     }
-
-    return invoked;
   }
 }
